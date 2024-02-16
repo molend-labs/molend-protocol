5613b1231dbc448d823a897ee245d4011d031c7e
molendtoby <159501549+molendtoby@users.noreply.github.com>
2024-02-16 11:54:34 +0800

    testnet deployment

--- markets/mode/index.ts
+++ markets/mode/index.ts
@@ -29,7 +29,6 @@ export const ModeConfig: IModeConfiguration = {
     [eModeNetwork.mode]: {
       USDT: '0x5306023Eb69ee9dDd5e8a54eA63007f7D984071F',
       WETH: '0xcc9ffcfBDFE629e9C62776fF01a75235F466794E',
-      WBTC: '0x896088e846cCE3751Aa6F6E8001C36aA1b5FC57a',
     },
   },
 };
